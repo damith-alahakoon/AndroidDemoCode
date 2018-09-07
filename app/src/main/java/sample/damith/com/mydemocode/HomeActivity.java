d36ea798dff5eceda145a196e2675cdf224724c9
Damith Alahakoon <damithalahakoon@gmail.com>
2018-09-07 12:12:27 +0530

    Add Validation activity

--- app/src/main/java/sample/damith/com/mydemocode/HomeActivity.java
+++ app/src/main/java/sample/damith/com/mydemocode/HomeActivity.java
@@ -1,7 +1,9 @@
 package sample.damith.com.mydemocode;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +11,8 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+    public void inputValidation(View view){
+        startActivity(new Intent(HomeActivity.this,ValidationActivity.class));
     }
 }
